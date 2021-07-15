47d38f75e06616e176d225f149ca81ca67e4b493
MrJazzpa <walisonmichael8@gmail.com>
2021-07-15 07:23:30 +0100

    dashboard fix

--- server/routes/viewsRoute/users.js
+++ server/routes/viewsRoute/users.js
@@ -32,7 +32,7 @@ router.get("/products", (req, res) => {
 });
 
 router.get("/dashboard", ensureAuthenticated, (req, res) => {
-  res.render("dashboard");
+  res.render("users/dashboard");
 });
 
 module.exports = router;
